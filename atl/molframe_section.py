fc7c8c8c3fa004e548ad504f51ca89589da03933
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-02-28 12:00:46 +0100

    added dihedrals and impropers class

--- atl/molframe_section.py
+++ atl/molframe_section.py
@@ -47,13 +47,13 @@ class AnglesBlock(MolFrameBlock):
         MolFrameBlock.__init__(self, 'Angles')
 
 
-class Dihedrals(MolFrameBlock):
+class DihedralsBlock(MolFrameBlock):
 
     def __init__(self):
         MolFrameBlock.__init__(self, 'Dihedrals')
 
 
-class Impropers(MolFrameBlock):
+class ImpropersBlock(MolFrameBlock):
 
     def __init__(self):
         MolFrameBlock.__init__(self, 'Impropers')
@@ -146,30 +146,81 @@ class Angle:
         return out
 
 
+class Dihedral:
+
+    def __init__(self, did, dtype, aid_i, aid_j, aid_k, aid_l, label=''):
+
+        try:
+            self.did = int_ge_zero(did)        # angle id
+            self.dtype = int_ge_zero(dtype)    # angle type
+            self.aid_i = int_ge_zero(aid_i)    # atom id (i)
+            self.aid_j = int_ge_zero(aid_j)    # atom id (j)
+            self.aid_k = int_ge_zero(aid_k)    # atom id (k)
+            self.aid_l = int_ge_zero(aid_l)    # atom id (l)
+            self.label = str(label)
+
+        except ValueError:
+            raise AssertionError("Unexpected value for Dihedral!")
+
+    def __str__(self):
+        out = ''
+        for attribute in [self.did, self.dtype, self.aid_i, self.aid_j, self.aid_j, self.aid_l, self.label]:
+            out += str(attribute) + ' '
+        return out
+
+
+class Improper:
+
+    def __init__(self, iid, itype, aid_i, aid_j, aid_k, aid_l, label=''):
+
+        try:
+            self.iid = int_ge_zero(iid)        # angle id
+            self.itype = int_ge_zero(itype)    # angle type
+            self.aid_i = int_ge_zero(aid_i)    # atom id (i)
+            self.aid_j = int_ge_zero(aid_j)    # atom id (j)
+            self.aid_k = int_ge_zero(aid_k)    # atom id (k)
+            self.aid_l = int_ge_zero(aid_l)    # atom id (l)
+            self.label = str(label)
+
+        except ValueError:
+            raise AssertionError("Unexpected value for Improper!")
+
+    def __str__(self):
+        out = ''
+        for attribute in [self.iid, self.itype, self.aid_i, self.aid_j, self.aid_j, self.aid_l, self.label]:
+            out += str(attribute) + ' '
+        return out
+
+
+
+
+
+
 if __name__ == '__main__':
 
     atom1 = Atom(aid=1, mid=1, atype=1, q=0, x=0.3, y=0, z=0, imx=0, imy=0, imz=0, label='')
     atom2 = Atom(aid=2, mid=1, atype=1, q=0, x=0.5, y=0, z=0, imx=0, imy=0, imz=0, label='')
-
     atoms_block = AtomsBlock()
     atoms_block.add(atom1)
     atoms_block.add(atom2)
     print (atoms_block)
 
-
     bond1 = Bond(bid=1, btype=1, aid_i=1, aid_j=2)
     bond2 = Bond(bid=2, btype=1, aid_i=1, aid_j=2)
-
     bonds_block = BondsBlock()
     bonds_block.add(bond1)
     bonds_block.add(bond2)
     print (bonds_block)
 
     angle1 = Angle(anid=1, antype=1, aid_i=2, aid_j=4, aid_k=5, label=' # water angle')
-
     angles_block = AnglesBlock()
     angles_block.add(angle1)
     print (angles_block)
+
+    dihedral1 = Dihedral(did=1, dtype=1, aid_i=1, aid_j=2, aid_k=3, aid_l=4)
+    dihedral_block = DihedralsBlock()
+    dihedral_block.add(dihedral1)
+    print (dihedral_block)
 
     # Atoms = MolFrameBlock('Atoms')
     # Atoms.add()
