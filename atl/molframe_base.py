abdfd60831eb2ba0bcfa6ed57ea23ed32a222deb
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-03-05 10:48:14 +0100

    updated names of classes and input argument, more readable

--- atl/molframe_base.py
+++ atl/molframe_base.py
@@ -3,11 +3,11 @@ Defining molecular sections meta-class and subsequent classes
 
 """
 
-from abc import	ABCMeta, abstractmethod
+# from abc import	ABCMeta, abstractmethod
 from atl.error import int_ge_zero, float_ge_zero
 
 
-class MolFrameSection:
+class MolecularFrameSection:
     """
     Defining base class for each molecular frame section.
     """
@@ -37,41 +37,41 @@ class MolFrameSection:
         return block_object
 
 
-class AtomsSection(MolFrameSection):
+class AtomsSection(MolecularFrameSection):
     def __init__(self):
-        MolFrameSection.__init__(self, "Atoms")
+        MolecularFrameSection.__init__(self, "Atoms")
 
 
-class BondsSection(MolFrameSection):
+class BondsSection(MolecularFrameSection):
     def __init__(self):
-        MolFrameSection.__init__(self, "Bonds")
+        MolecularFrameSection.__init__(self, "Bonds")
 
 
-class AnglesSection(MolFrameSection):
+class AnglesSection(MolecularFrameSection):
     def __init__(self):
-        MolFrameSection.__init__(self, "Angles")
+        MolecularFrameSection.__init__(self, "Angles")
 
 
-class DihedralsSection(MolFrameSection):
+class DihedralsSection(MolecularFrameSection):
     def __init__(self):
-        MolFrameSection.__init__(self, "Dihedrals")
+        MolecularFrameSection.__init__(self, "Dihedrals")
 
 
-class ImpropersSection(MolFrameSection):
+class ImpropersSection(MolecularFrameSection):
     def __init__(self):
-        MolFrameSection.__init__(self, "Impropers")
+        MolecularFrameSection.__init__(self, "Impropers")
 
 
-class MassesSection(MolFrameSection):
-
-    def __init__(self):
-        MolFrameSection.__init__(self, 'Masses')
-
-
-class BoxSection(MolFrameSection):
+class MassesSection(MolecularFrameSection):
 
     def __init__(self):
-        MolFrameSection.__init__(self, 'Box')
+        MolecularFrameSection.__init__(self, 'Masses')
+
+
+class BoxSection(MolecularFrameSection):
+
+    def __init__(self):
+        MolecularFrameSection.__init__(self, 'Box')
         self.items = None
 
     def add(self, section):
@@ -84,10 +84,10 @@ class BoxSection(MolFrameSection):
         return str(self.sections)
 
 
-class MolTypeSection(MolFrameSection):
+class MolTypeSection(MolecularFrameSection):
 
     def __init__(self):
-        MolFrameSection.__init__(self, 'MolType')
+        MolecularFrameSection.__init__(self, 'MolType')
         self.items = None
 
     def add(self, section):
@@ -102,11 +102,11 @@ class MolTypeSection(MolFrameSection):
 
 class Atom:
 
-    def __init__(self, aid, mid, typ, q, x, y, z, imx=0, imy=0, imz=0, label=''):
+    def __init__(self, atom_id, molecule_id, atom_type, q, x, y, z, imx=0, imy=0, imz=0, label=''):
         try:
-            self.aid = int_ge_zero(aid)     # atom id
-            self.mid = int_ge_zero(mid)     # molecule id
-            self.typ = int_ge_zero(typ)     # atom type
+            self.atom_id = int_ge_zero(atom_id)          # atom id
+            self.molecule_id = int_ge_zero(molecule_id)  # molecule id
+            self.atom_type = int_ge_zero(atom_type)      # atom type
             self.q = float(q)        # atom charge
             self.x = float(x)        # x-coordinate
             self.y = float(y)        # y-coordinate
@@ -121,7 +121,7 @@ class Atom:
 
     def __str__(self):
         out = ''
-        for attribute in [self.aid, self.mid, self.typ, self.q, self.x, self.y, self.z,
+        for attribute in [self.atom_id, self.molecule_id, self.atom_type, self.q, self.x, self.y, self.z,
                           self.imx, self.imy, self.imz, self.label]:
             out += str(attribute) + ' '
         return out
@@ -129,10 +129,10 @@ class Atom:
 
 class Bond:
 
-    def __init__(self, bid, typ, aid_i, aid_j, label=''):
+    def __init__(self, bond_id, bond_type, aid_i, aid_j, label=''):
         try:
-            self.bid = int_ge_zero(bid)      # bond id
-            self.typ = int_ge_zero(typ)      # bond type
+            self.bond_id = int_ge_zero(bond_id)      # bond id
+            self.bond_type = int_ge_zero(bond_type)  # bond type
             self.aid_i = int_ge_zero(aid_i)  # atom id (i)
             self.aid_j = int_ge_zero(aid_j)  # atom id (j)
             self.label = str(label)          # bond label
@@ -142,17 +142,17 @@ class Bond:
 
     def __str__(self):
         out = ''
-        for attribute in [self.bid, self.typ, self.aid_i, self.aid_j, self.label]:
+        for attribute in [self.bond_id, self.bond_type, self.aid_i, self.aid_j, self.label]:
             out += str(attribute) + ' '
         return out
 
 
 class Angle:
 
-    def __init__(self, anid, typ, aid_i, aid_j, aid_k, label=''):
+    def __init__(self, angle_id, angle_type, aid_i, aid_j, aid_k, label=''):
         try:
-            self.anid = int_ge_zero(anid)      # angle id
-            self.typ = int_ge_zero(typ)        # angle type
+            self.angle_id = int_ge_zero(angle_id)      # angle id
+            self.angle_type = int_ge_zero(angle_type)  # angle type
             self.aid_i = int_ge_zero(aid_i)    # atom id (i)
             self.aid_j = int_ge_zero(aid_j)    # atom id (j)
             self.aid_k = int_ge_zero(aid_k)    # atom id (k)
@@ -163,17 +163,17 @@ class Angle:
 
     def __str__(self):
         out = ''
-        for attribute in [self.anid, self.typ, self.aid_i, self.aid_j, self.aid_j, self.label]:
+        for attribute in [self.angle_id, self.angle_type, self.aid_i, self.aid_j, self.aid_j, self.label]:
             out += str(attribute) + ' '
         return out
 
 
 class Dihedral:
 
-    def __init__(self, did, typ, aid_i, aid_j, aid_k, aid_l, label=''):
+    def __init__(self, dihedral_id, dihedral_type, aid_i, aid_j, aid_k, aid_l, label=''):
         try:
-            self.did = int_ge_zero(did)        # angle id
-            self.typ = int_ge_zero(typ)        # angle type
+            self.dihedral_id = int_ge_zero(dihedral_id)        # angle id
+            self.dihedral_type = int_ge_zero(dihedral_type)    # angle type
             self.aid_i = int_ge_zero(aid_i)    # atom id (i)
             self.aid_j = int_ge_zero(aid_j)    # atom id (j)
             self.aid_k = int_ge_zero(aid_k)    # atom id (k)
@@ -185,17 +185,17 @@ class Dihedral:
 
     def __str__(self):
         out = ''
-        for attribute in [self.did, self.typ, self.aid_i, self.aid_j, self.aid_j, self.aid_l, self.label]:
+        for attribute in [self.dihedral_id, self.dihedral_type, self.aid_i, self.aid_j, self.aid_j, self.aid_l, self.label]:
             out += str(attribute) + ' '
         return out
 
 
 class Improper:
 
-    def __init__(self, iid, typ, aid_i, aid_j, aid_k, aid_l, label=''):
+    def __init__(self, improper_id, improper_type, aid_i, aid_j, aid_k, aid_l, label=''):
         try:
-            self.iid = int_ge_zero(iid)        # angle id
-            self.itype = int_ge_zero(typ)      # angle type
+            self.improper_id = int_ge_zero(improper_id)        # angle id
+            self.improper_type = int_ge_zero(improper_type)      # angle type
             self.aid_i = int_ge_zero(aid_i)    # atom id (i)
             self.aid_j = int_ge_zero(aid_j)    # atom id (j)
             self.aid_k = int_ge_zero(aid_k)    # atom id (k)
@@ -207,25 +207,25 @@ class Improper:
 
     def __str__(self):
         out = ''
-        for attribute in [self.iid, self.itype, self.aid_i, self.aid_j, self.aid_j, self.aid_l, self.label]:
+        for attribute in [self.improper_id, self.improper_type, self.aid_i, self.aid_j, self.aid_j, self.aid_l, self.label]:
             out += str(attribute) + ' '
         return out
 
 
 class Mass:
 
-    def __init__(self, msid, mass, label=''):
+    def __init__(self, mass_id, mass, label=''):
         try:
-            self.msid = int_ge_zero(msid)    # mass id
-            self.mass = float_ge_zero(mass)  # mass value
-            self.label = str(label)          # mass label
+            self.mass_id = int_ge_zero(mass_id)  # mass id
+            self.mass = float_ge_zero(mass)      # mass value
+            self.label = str(label)              # mass label
 
         except ValueError:
             raise AssertionError("Unexpected value for Mass!")
 
     def __str__(self):
         out = ''
-        for attribute in [self.msid, self.mass, self.label]:
+        for attribute in [self.mass_id, self.mass, self.label]:
             out += str(attribute) + ' '
         return out
 
@@ -279,48 +279,48 @@ class MolType:
 
 if __name__ == '__main__':
 
-    atom1 = Atom(aid=1, mid=1, typ=1, q=0, x=0.3, y=0, z=0, imx=0, imy=0, imz=0, label='')
-    atom2 = Atom(aid=2, mid=1, typ=1, q=0, x=0.5, y=0, z=0, imx=0, imy=0, imz=0, label='')
+    atom1 = Atom(atom_id=1, molecule_id=1, atom_type=1, q=0, x=0.3, y=0, z=0, imx=0, imy=0, imz=0, label='')
+    atom2 = Atom(atom_id=2, molecule_id=1, atom_type=1, q=0, x=0.5, y=0, z=0, imx=0, imy=0, imz=0, label='')
     atoms_block = AtomsSection()
     atoms_block.add(atom1)
     atoms_block.add(atom2)
     print (atoms_block)
 
-    bond1 = Bond(bid=1, typ=1, aid_i=1, aid_j=2)
-    bond2 = Bond(bid=2, typ=1, aid_i=1, aid_j=2)
+    bond1 = Bond(bond_id=1, bond_type=1, aid_i=1, aid_j=2)
+    bond2 = Bond(bond_id=2, bond_type=1, aid_i=1, aid_j=2)
     bonds_block = BondsSection()
     bonds_block.add(bond1)
     bonds_block.add(bond2)
     print (bonds_block)
 
-    angle1 = Angle(anid=1, typ=1, aid_i=2, aid_j=4, aid_k=5, label=' # water angle')
+    angle1 = Angle(angle_id=1, angle_type=1, aid_i=2, aid_j=4, aid_k=5, label=' # water angle')
     angles_block = AnglesSection()
     angles_block.add(angle1)
     print (angles_block)
 
-    dihedral1 = Dihedral(did=1, typ=1, aid_i=1, aid_j=2, aid_k=3, aid_l=4)
+    dihedral1 = Dihedral(dihedral_id=1, dihedral_type=1, aid_i=1, aid_j=2, aid_k=3, aid_l=4)
     dihedral_block = DihedralsSection()
     dihedral_block.add(dihedral1)
     print (dihedral_block)
 
-    improper1 = Improper(iid=1, typ=1, aid_i=1, aid_j=2, aid_k=3, aid_l=4)
+    improper1 = Improper(improper_id=1, improper_type=1, aid_i=1, aid_j=2, aid_k=3, aid_l=4)
     improper_block = ImpropersSection()
     improper_block.add(improper1)
     print (improper_block)
 
     box1 = Box(xlo=1, xhi=2, ylo=-1, yhi=2.3, zlo=92, zhi=23)
-    box_block = MolFrameSection.make("BoxSection")
+    box_block = MolecularFrameSection.make("BoxSection")
     box_block.add(box1)
     print (box_block)
 
-    mass1 = Mass(msid=1, mass=2.3, label='O')
-    mass2 = Mass(msid=2, mass=0.33, label='H')
+    mass1 = Mass(mass_id=1, mass=2.3, label='O')
+    mass2 = Mass(mass_id=2, mass=0.33, label='H')
     masses_block = MassesSection()
     masses_block.add(mass1)
     masses_block.add(mass2)
     print (masses_block)
 
-    moltype1 =  MolType(atoms=[100, 2], bonds=[21, 1])
+    moltype1 = MolType(atoms=[100, 2], bonds=[21, 1])
     moltype_block = MolTypeSection()
     moltype_block.add(moltype1)
     print (moltype_block)
