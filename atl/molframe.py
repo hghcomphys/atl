1991f94e0906dee56de39191007f873000f6ae8c
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-03-05 12:50:38 +0100

    import ase cell info into Box class (only orthogonal)

--- atl/molframe.py
+++ atl/molframe.py
@@ -10,17 +10,19 @@ class MolecularFrame:
     def __init__(self):
         self.sections = []
 
-    def import_ase(self, ase_structure):
-        mf_sections = AdaptorASE().get_molecular_frame_sections(ase_structure)
-        for sec in mf_sections:
-            self.sections.append(sec)
-
     def __str__(self):
         out = 'Molecular Frame'
         for sec in self.sections:
             out += str(sec) + '\n'
         return out
 
+    def import_ase(self, ase_boject):
+        mf_sections = AdaptorASE(ase_boject).get_molecular_frame_sections()
+        for sec in mf_sections:
+            self.sections.append(sec)
+
+    def export_xyz(self):
+        pass
 
 
 # ==========================================================================================
@@ -30,7 +32,6 @@ if __name__ == '__main__':
 
     from ase.build import bulk
     crys = bulk('Cu', 'fcc', a=3.6, orthorhombic=True)
-
     from ase.build.supercells import make_supercell
     import numpy as np
     P = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]) * 10
--- atl/molframe_ase.py
+++ atl/molframe_ase.py
@@ -7,23 +7,28 @@ from molframe_base import *
 
 class AdaptorASE:
 
-    def __init__(self):
-        pass
+    def __init__(self, ase_object):
+        self.ase_object= ase_object   # ase object including structural info
+        self.sections = []  # list of molecular frame sections
 
-    def get_molecular_frame_sections(self, ase_structure):
+    def get_molecular_frame_sections(self):
 
-        mf_sections = []
+        # Box Section
+        box_sections = BoxSection()
+        cell_info = self.ase_object.get_cell().diagonal()  # WARNING: only works for orthogonal cell!
+        box = Box(xlo=0, xhi=cell_info[0], ylo=0, yhi=cell_info[1], zlo=0, zhi=cell_info[2])
+        self.sections.append(box)
 
-        # importing sections from ase structure
-        atom_positions = ase_structure.get_positions()
-        atom_labels = ase_structure.get_chemical_symbols()
+        # Atoms Section
+        atom_positions = self.ase_object.get_positions()
+        atom_labels = self.ase_object.get_chemical_symbols()
         atoms_section = AtomsSection()
         for aid, pos, label in zip(range(len(atom_labels)), atom_positions, atom_labels):
             atom = Atom(atom_id=aid+1, molecule_id=0, atom_type=0, q=0.0, x=pos[0], y=pos[1], z=pos[0], label=label)
             atoms_section.add(atom)
-        mf_sections.append(atoms_section)
+        self.sections.append(atoms_section)
 
-        # returning a list of molecular frame sections
-        return mf_sections
+        # returning the list of molecular frame sections
+        return self.sections
 
 
