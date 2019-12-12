0eef3a9433ef63884c293a1952e829db5ba7f422
Hossein <hgh.comphys@gmail.com>
2019-12-12 18:08:34 +0100

    add python wrapper calculate_zden for calc_zden_fort.f90

---
+++ atl/zden.py
@@ -0,0 +1,17 @@
+"""
+Python wrapper for calling subroutine from zden_fort.90 using f2py.
+"""
+
+def calculate_zden(file_name, sel_type, zlim, nz=100, frames=[1, 1000000, 1]):
+    """
+    This subroutine calculates radial distribution function (RDF) between atoms
+    (at this moment atoms with type) from .xyz output format via averaging over
+    specified frames.
+    """
+    import os
+    # making .so file (ih has to be automated)
+    # os.system("f2py -c rdf_fort.f90 -m rdf_fort")
+    import atl.zden_fort # !!! (makefile needed)
+
+    # calling Fortran subroutine
+    atl.zden_fort.calc_zden_fort(file_name, sel_type, zlim, nz, frames)
