14943ebe5003bf3e61e51095b6ebee4d9ef4da74
Hossein <hgh.comphys@gmail.com>
2019-12-29 12:25:55 +0100

    add dump_angle argument (default is False)

--- atl/hbond.py
+++ atl/hbond.py
@@ -3,7 +3,7 @@ Python wrapper for calling subroutine from hbond_fort.90 using f2py.
 """
 
 def calculate_hbond(file_name, pbc_box, sel_type=['O', 'H', 'O'], 
-    criteria=[3.5, 2.5, 30.0], z_confined=False, frames=[1, 1000000, 1]):
+    criteria=[3.5, 2.5, 30.0], z_confined=False, dump_angle=False, frames=[1, 1000000, 1]):
 
     """
     This subroutine calculates total number of hydrogen bonds, number of donor and acceptor 
@@ -16,4 +16,4 @@ def calculate_hbond(file_name, pbc_box, sel_type=['O', 'H', 'O'],
 
     # calling Fortran subroutine
     atl.hbond_fort.calc_hbond_fort(file_name, sel_type[0], sel_type[1], 
-        sel_type[2], criteria, pbc_box, z_confined, frames)
+        sel_type[2], criteria, pbc_box, z_confined, dump_angle, frames)
